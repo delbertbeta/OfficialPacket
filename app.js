60181c94df2e856a3f7f7075aba3d47c938064a7
delbertbeta <delbertbeta@live.com>
2018-07-10 21:14:01 +0800

    Make the login works well.

--- app.js
+++ app.js
@@ -5,31 +5,27 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log(res);
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+        wx.request({
+          url: 'https://dsn.apizza.net/mock/be28017e647358c1dfe41415fd8a2ab6/login',
+          data: {
+            code: res.code
+          },
+          method: 'POST',
+          dataType: 'json',
+          success: res => {
+            this.globalData.openId = res.data.openId
+          }
+        })
+      },
+      fail: res => {
+        wx.switchTab({
+          url: '/pages/index/index',
+        })
       }
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openId: null
   }
 })
