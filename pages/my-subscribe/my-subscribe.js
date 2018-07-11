69fe696e44c4f7fa130d5a46fbeb9f0c954f2aa6
delbertbeta <delbertbeta@live.com>
2018-07-11 21:24:27 +0800

    Make the my-subscribe page work well.

--- pages/my-subscribe/my-subscribe.js
+++ pages/my-subscribe/my-subscribe.js
@@ -1,18 +1,40 @@
 // pages/my-subscribe/my-subscribe.js
+const app = getApp();
+const api = require('../../apis/api.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    accounts: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    api.subscribe.subscribe(app.globalData.openId, api.subscribe.orderType.recentlySubscribed, res => {
+      this.setData({
+        accounts: res.data.map((v) => {
+          v.subscribed = true;
+          return v;
+        })
+      })
+    });
+  },
+
+  makeOrCancel(e) {
+    const index = e.currentTarget.dataset.index;
+    const targetAccount = this.data.accounts[index];
+    const handle = targetAccount.subscribed ? api.subscribe.cancelSubscribe : api.subscribe.makeSubscribe;
+    handle(app.globalData.openId, targetAccount.id, res => {
+      const targetString = 'accounts[' + index + '].subscribed';
+      const data = {};
+      data[targetString] = !targetAccount.subscribed
+      this.setData(data);
+    })
   },
 
   /**
